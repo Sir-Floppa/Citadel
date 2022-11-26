4cc291f103435a68970be184fccdbf4b683de817
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2022-11-26 15:38:44 -0300

    added character fields

--- src/app/new-character/new-character.component.ts
+++ src/app/new-character/new-character.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-character',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCharacterComponent implements OnInit {
 
-  constructor() { }
+  charName: string = '';
+  charBirth: string = '';
+  charOrigin: string = '';
+  charRes: string = '';
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  acceptBtn(): void {
+    this.charName = (<HTMLInputElement>document.getElementById("nameField")).value;
+    this.charBirth = (<HTMLInputElement>document.getElementById("birthField")).value;
+    this.charOrigin = (<HTMLInputElement>document.getElementById("originField")).value;
+    this.charRes = (<HTMLInputElement>document.getElementById("resField")).value;
+
+    console.log("NAME", this.charName);
+    console.log("BIRTH", this.charBirth);
+    console.log("ORIGIN", this.charOrigin);
+    console.log("RESUME", this.charRes);
+  }
+
+  cancelBtn(): void {
+    this.router.navigate(['./../..'], {relativeTo: this.route})
   }
 
 }
